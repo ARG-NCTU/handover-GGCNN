e46cd7170e63c22fc821be9628cbe5451851d312
Alex1114 <pokai.eed05@g2.nctu.edu.tw>
2021-06-21 20:07:02 +0800

    hand_and_body

--- git_pull.sh
+++ git_pull.sh
@@ -22,12 +22,38 @@ echo "-----------------------------------------------------------------------"
 echo "-------------------------pull darknet_ros----------------------"
 echo "-----------------------------------------------------------------------"
 cd ~/mm-medical/catkin_ws/src/darknet_ros
-git checkout master
+git checkout main
 git pull
 
 CONFLICTS=$(git ls-files -u | wc -l)
 if [ "$CONFLICTS" -gt 0 ] ; then
    echo "There is conflict in darknet_ros. Aborting"
+   return 1
+fi
+
+echo "-----------------------------------------------------------------------"
+echo "-------------------------pull egohands_ros----------------------"
+echo "-----------------------------------------------------------------------"
+cd ~/mm-medical/catkin_ws/src/egohands_ros
+git checkout main
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in egohands_ros. Aborting"
+   return 1
+fi
+
+echo "-----------------------------------------------------------------------"
+echo "-------------------------pull bodyparts_ros----------------------"
+echo "-----------------------------------------------------------------------"
+cd ~/mm-medical/catkin_ws/src/bodyparts_ros
+git checkout main
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in bodyparts_ros. Aborting"
    return 1
 fi
 
