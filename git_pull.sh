4e534365f1c48e2eb6b0f63784537d8acba32af7
Alex1114 <pokai.eed05@g2.nctu.edu.tw>
2021-06-22 16:18:03 +0800

    GGCNN

--- git_pull.sh
+++ git_pull.sh
@@ -21,8 +21,8 @@ fi
 echo "-----------------------------------------------------------------------"
 echo "-------------------------pull darknet_ros----------------------"
 echo "-----------------------------------------------------------------------"
-cd ~/mm-medical/catkin_ws/src/darknet_ros
-git checkout main
+cd ~/handover-GGCNN/catkin_ws/src/darknet_ros
+git checkout master
 git pull
 
 CONFLICTS=$(git ls-files -u | wc -l)
@@ -34,8 +34,7 @@ fi
 echo "-----------------------------------------------------------------------"
 echo "-------------------------pull egohands_ros----------------------"
 echo "-----------------------------------------------------------------------"
-cd ~/mm-medical/catkin_ws/src/egohands_ros
-git checkout main
+cd ~/handover-GGCNN/catkin_ws/src/egohands_ros
 git pull
 
 CONFLICTS=$(git ls-files -u | wc -l)
@@ -47,13 +46,24 @@ fi
 echo "-----------------------------------------------------------------------"
 echo "-------------------------pull bodyparts_ros----------------------"
 echo "-----------------------------------------------------------------------"
-cd ~/mm-medical/catkin_ws/src/bodyparts_ros
-git checkout main
+cd ~/handover-GGCNN/catkin_ws/src/bodyparts_ros
 git pull
 
 CONFLICTS=$(git ls-files -u | wc -l)
 if [ "$CONFLICTS" -gt 0 ] ; then
    echo "There is conflict in bodyparts_ros. Aborting"
+   return 1
+fi
+
+echo "-----------------------------------------------------------------------"
+echo "-------------------------pull ggcnn_humanseg_ros----------------------"
+echo "-----------------------------------------------------------------------"
+cd ~/handover-GGCNN/catkin_ws/src/ggcnn_humanseg_ros
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in ggcnn_humanseg_ros. Aborting"
    return 1
 fi
 
